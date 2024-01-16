9929733478dded2524d0a424a8e9c99a23d85554
jcmcgown <jcmcgown47@gmail.com>
2024-01-16 13:47:17 -0500

    update

--- day_3/coding_exercises/bmi_2.0.py
+++ day_3/coding_exercises/bmi_2.0.py
@@ -1,2 +1,20 @@
 # Exercise 9 - BMI 2.0
 
+height = float(input())
+weight = int(input())
+
+weight_as_int = int(weight)
+height_as_float = float(height)
+
+bmi = weight_as_int / height_as_float ** 2
+
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif 25 > bmi >= 18.5:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif 30 > bmi >= 25:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif 35 > bmi >= 30:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
--- day_3/coding_exercises/leap_year.py
+++ day_3/coding_exercises/leap_year.py
@@ -1,2 +1,14 @@
 # Exercise 10 - Leap Year
 
+year = int(input())
+
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
+else:
+  print("Not leap year")
